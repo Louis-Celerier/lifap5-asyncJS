6aa404878e6d9adca347b6b27aed1fce3c713030
= <=>
2020-05-02 13:48:44 +0200

    Final Obligatoire

--- js/vues.js
+++ js/vues.js
@@ -270,16 +270,21 @@ function renderMyQuizzes() {
           <h5>${ordre.description}</h5>
           <h6>#${ordre.quiz_id}</h6>
           </div>`;
-        ordre.questions_ids.map((question_id) => {
-          const url2 = `${state.serverUrl}/quizzes/${ordre.quiz_id}/questions/${question_id}`;
-          fetch(url2, { method: 'GET', headers: state.headers })
-          .then(filterHttpResponse)
-          .then((data2) => {
+        const url2 = `${state.serverUrl}/quizzes/${ordre.quiz_id}/questions/`;
+        fetch(url2, { method: 'GET', headers: state.headers })
+        .then(filterHttpResponse)
+        .then((data2) => {
+            let indice = 0
             let div = document.getElementById(ordre.quiz_id);
+            ordre.questions_ids.map((question_id) => {
             div.innerHTML += `<hr/><p>${data2.sentence}</p><hr/>`;
-            data2.propositions.map((contenu) => {
-              div.innerHTML += `<p><i class="material-icons">chevron_right</i>  ${contenu.content}</p>`;
+            data2[indice].propositions.map((contenu) => {
+              if(contenu.proposition_id == data2[indice].correct_propositions_number)
+                div.innerHTML += `<p class="teal-text text-lighten-2"><i class="material-icons">done</i>  ${contenu.content}</p>`;
+              else
+                div.innerHTML += `<p><i class="material-icons">chevron_right</i>  ${contenu.content}</p>`;
             });
+            indice++;
           });
         });
         div_question.innerHTML += `<hr/><div>
