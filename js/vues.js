fbcf0cabd5567ce30eff35fc80c4365b98b301ed
= <=>
2020-05-03 15:18:14 +0200

    Reponse #2

--- js/vues.js
+++ js/vues.js
@@ -307,6 +307,7 @@ function renderMyAnswer() {
   console.debug(`@renderMyAnswer()`);
   let bloc_reponse = document.getElementById('id-my-answers');
   let div_reponse = bloc_reponse.children[0];
+  div_reponse.innerHTML = "";
   if(state.user) {
     const url = `${state.serverUrl}/users/answers`;
     fetch(url, { method: 'GET', headers: state.headers })
@@ -314,16 +315,16 @@ function renderMyAnswer() {
     .then((data) => {
       data.map((data2) => {
         div_reponse.innerHTML += `<div  id="${data2.quiz_id}"><h3>${data2.title}</h3><h5>${data2.description}</h5><div>`;
-        data2.answers.map((reponse) => {
-          const url2 = `${state.serverUrl}/quizzes/${data2.quiz_id}/questions/${reponse.question_id}/`;
-          fetch(url2, { method: 'GET', headers: state.headers })
-          .then(filterHttpResponse)
-          .then((data3) => {
-            div_reponse = document.getElementById(data2.quiz_id);
-            div_reponse.innerHTML += `<hr/><p>${data3.sentence}</p><hr/>`;
-            data3.propositions.map((contenu) => {
-              if(reponse.proposition_id == contenu.proposition_id){
-                if(reponse.proposition_id == data3.correct_propositions_number)
+        const url2 = `${state.serverUrl}/quizzes/${data2.quiz_id}/questions/`;
+        fetch(url2, { method: 'GET', headers: state.headers })
+        .then(filterHttpResponse)
+        .then((data3) => {
+          div_reponse = document.getElementById(data2.quiz_id);
+          data3.map((question) => {
+            div_reponse.innerHTML += `<hr/><p>${question.sentence}</p><hr/>`;
+            question.propositions.map((contenu) => {
+              if(data2.answers[question.question_id].proposition_id == contenu.proposition_id){
+                if(data2.answers[question.question_id].proposition_id == question.correct_propositions_number)
                   div_reponse.innerHTML += `<p class="teal-text text-lighten-2"><i class="material-icons">check</i> ${contenu.content}</p>`;
                 else
                   div_reponse.innerHTML += `<p class="red-text text-lighten-2"><i class="material-icons">clear</i>  ${contenu.content}</p>`;
